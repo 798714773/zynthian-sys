d1de264536e976fe62a9820c76173c65c09fb487
798714773 <798714773@qq.com>
2025-07-17 22:47:41 +0800

    改回原来的第三方源

--- scripts/setup_system_raspioslite_64bit_bookworm.sh
+++ scripts/setup_system_raspioslite_64bit_bookworm.sh
@@ -80,13 +80,9 @@ fi
 #------------------------------------------------
 
 # deb-multimedia repo
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-multimedia/ bookworm main non-free" >> /etc/apt/sources.list
-
-wget https://mirrors.tuna.tsinghua.edu.cn/debian-multimedia/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
-sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-
+echo "deb https://www.deb-multimedia.org bookworm main non-free" >> /etc/apt/sources.list
 apt-get -y update -oAcquire::AllowInsecureRepositories=true
-
+apt-get -y --allow-unauthenticated  install deb-multimedia-keyring
 
 # KXStudio
 wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_11.1.0_all.deb
