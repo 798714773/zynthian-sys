fd678808c2b03ad309e1e443239439288ce6c459
798714773 <798714773@qq.com>
2025-07-18 17:06:29 +0800

    修改deb-multimedia清华源

--- scripts/setup_system_raspioslite_64bit_bookworm.sh
+++ scripts/setup_system_raspioslite_64bit_bookworm.sh
@@ -75,9 +75,12 @@ apt-get -y install apt-transport-https rpi-update sudo software-properties-commo
 #------------------------------------------------
 
 # deb-multimedia repo
-echo "deb https://www.deb-multimedia.org bookworm main non-free" >> /etc/apt/sources.list
+# echo "deb https://www.deb-multimedia.org bookworm main non-free" >> /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-multimedia/ bookworm main non-free" >> /etc/apt/sources.list
+wget https://mirrors.tuna.tsinghua.edu.cn/debian-multimedia/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2024.9.1_all.deb
+dpkg -i deb-multimedia-keyring_2024.9.1_all.deb
 apt-get -y update -oAcquire::AllowInsecureRepositories=true
-apt-get -y --allow-unauthenticated  install deb-multimedia-keyring
+#apt-get -y --allow-unauthenticated  install deb-multimedia-keyring
 
 # KXStudio
 wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_11.2.0_all.deb
